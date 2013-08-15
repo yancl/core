6aff054f0910814f937e9a17c00df97e0bea7200
yancl <kmoving@gmail.com>
2013-08-15 21:28:25 +0800

    fix zookeeper client concurrent bug

--- zhwcore/client/zk_r.py
+++ zhwcore/client/zk_r.py
@@ -5,8 +5,8 @@ from threading import Lock
 class ZookeeperClient(KazooClient):
     _lock = Lock()
     def __getattr__(self, key):
-        with _lock:
-            def _(*args, **kwargs):
+        def _(*args, **kwargs):
+            with _lock:
                 func = getattr(self, key)
                 return func(*args, **kwargs)
         return _
