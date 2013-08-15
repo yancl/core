8a181d01c646ef8c124b209496026a5f5de06326
yancl <kmoving@gmail.com>
2013-08-15 21:25:17 +0800

    fix zk_r lock problem

--- zhwcore/client/zk_r.py
+++ zhwcore/client/zk_r.py
@@ -1,34 +1,12 @@
 from kazoo.client import KazooClient
 from threading import Lock
 
-lock = Lock()
 
 class ZookeeperClient(KazooClient):
+    _lock = Lock()
     def __getattr__(self, key):
-        with lock:
+        with _lock:
             def _(*args, **kwargs):
                 func = getattr(self, key)
                 return func(*args, **kwargs)
         return _
-
-if __name__ == '__main__':
-    #zk = KazooClient(hosts='127.0.0.1:2181')
-    zk = ZookeeperClient(hosts='127.0.0.1:2181')
-    zk.start()
-
-    # Ensure a path, create if necessary
-    zk.ensure_path("/my/favorite")
-
-    # Create a node with data
-    zk.create("/my/favorite/node", b"a value")
-    
-    @zk.ChildrenWatch("/my/favorite/node")
-    def watch_children(children):
-        print("Children are now: %s" % children)
-    # Above function called immediately, and from then on
-    
-    @zk.DataWatch("/my/favorite")
-    def watch_node(data, stat):
-        print("Version: %s, data: %s" % (stat.version, data.decode("utf-8")))
-    
-    zk.stop()
