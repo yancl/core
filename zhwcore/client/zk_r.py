a25637d16fd0f4024ac71f6af8773b216f7618ae
yancl <kmoving@gmail.com>
2013-08-16 16:24:12 +0800

    explore children changed watch api

--- zhwcore/client/zk_r.py
+++ zhwcore/client/zk_r.py
@@ -71,10 +71,16 @@ class ServiceAddrResovler(object):
     """
     def __init__(self, zk_addr, service_addr, callback=None):
         """
-        zk_address is zookeeper cluster hosts
-        service_addr is service address
+        @zk_addr: is zookeeper cluster hosts
+        @service_addr: is service address
             for example:
             /zhw/services/user/
+
+        @callback: called when the children changed
+        callback of the following form:
+
+        def callback(hosts):
+            pass
 
         """
         self._zk = ZookeeperClient(zk_addr)
@@ -82,22 +88,27 @@ class ServiceAddrResovler(object):
             raise Exception('service_addr must be end with "/", now:[%s]' % service_addr)
         self._service_addr = service_addr
 
+        self._callback = callback
+
         #init connection to zookeeper
         self._zk.start()
-        self._watch_children()
 
     def get_hosts(self):
         """
         get hosts of the service address contains
 
         """
-        hosts = []
         nodes = self._get_children()
-        for node in nodes:
-            host = self._get_value(self._service_addr + '/' + node)
-            if host:
-                hosts.append(host)
+        hosts = self._get_nodes_value(nodes)
         return hosts
+
+    def watch_children(self):
+        """
+        watch the children changed event
+        and call the callback
+
+        """
+        self._zk.ChildrenWatch(self._service_addr, self._children_changed)
 
     def _get_children(self):
         """
@@ -106,6 +117,18 @@ class ServiceAddrResovler(object):
         """
         children = self._zk.get_children(self._service_addr)
         return children
+
+    def _get_nodes_value(self, nodes):
+        """
+        get values stored in the nodes
+
+        """
+        hosts = []
+        for node in nodes:
+            host = self._get_value(self._service_addr + '/' + node)
+            if host:
+                hosts.append(host)
+        return hosts
 
     def _get_value(self, path):
         """
@@ -117,18 +140,11 @@ class ServiceAddrResovler(object):
         except NoNodeError:
             return None
 
-    def _watch_children(self):
-        """
-        watch the children changed event
-        and call the callback
-
-        """
-        self._zk.ChildrenWatch(self._service_addr, self._children_changed)
-
-
-    def _children_changed(children):
+    def _children_changed(self, children):
         """
         called when children changed
 
         """
-        print "Children are now: %s" % children
+        hosts = self._get_nodes_value(children)
+        if self._callback:
+            self._callback(hosts)
